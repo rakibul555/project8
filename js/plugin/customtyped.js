476be800d308cd96facc86752328f3c2e1fd17bf
Rakibul Islam <56691130+rakibul555@users.noreply.github.com>
2021-08-05 22:35:17 +0600

    Add files via upload

---
+++ js/plugin/customtyped.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    //Section section
+    //  var typed2 = new Typed('#typed2', {
+    // var typed = new Typed('.typed', {
+    //     strings: [
+    //         'Webdesigner',
+    //         'Webdeveloper',
+    //         'Psd to html ex.',
+    //         'Blog website ex.',
+    //     ],
+    //     loop: true,
+    //     typeSpeed: 30,
+    // });
+
+
+
+    var typed2 = new Typed('.typed', {
+        strings: [
+            'Webdesigner',
+            'Webdeveloper',
+            'Psd to html ex.',
+            'Blog website ex.',
+        ],
+        typeSpeed: 0,
+        backSpeed: 0,
+        fadeOut: false,
+        loop: true
+    });
+
+
+});
